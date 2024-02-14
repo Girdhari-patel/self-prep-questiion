233a637375c769b32e454d7b30ad9561514f61e7
Girdhari-patel <girdharip45@gmail.com>
2024-02-14 19:57:06 +0530

    imp ques khud se bnaya h

--- leetCode/1551.Minimum_Operations_to_Make_Array_Equal.cpp
+++ leetCode/1551.Minimum_Operations_to_Make_Array_Equal.cpp
@@ -41,3 +41,24 @@ public:
         return min_op;
     }
 };
+
+/*
+class Solution {
+public:
+    int minOperations(int n) {
+       int ans=0;
+       for(int i=0;i<n/2;i++)
+       {
+           ans+=n-((2*i) + 1);
+       }
+       return ans;
+    }
+};
+
+class Solution {
+public:
+    int minOperations(int n) {
+        return ( n/2 ) * ( n - (n/2));
+    }
+}; 
+*/
